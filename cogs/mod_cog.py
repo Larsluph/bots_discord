f3b8eb711c4ff7f661d89ddb5262a5969ac5d642
Larsluph <remarso59@sfr.fr>
2021-04-23 21:15:49 +0200

    disabled lockdown command + optimization

--- cogs/mod_cog.py
+++ cogs/mod_cog.py
@@ -1,7 +1,6 @@
 """\
 Cog to implement mod commands:
     - clear
-    - purge
     - send
     - kick
     - ban
@@ -11,10 +10,8 @@ Cog to implement mod commands:
 
 from typing import Optional
 
-import discord
+from discord import Member, TextChannel, User
 from discord.ext import commands
-
-from cogs import utils
 
 
 class Mod(commands.Cog, name="ModCog"):
@@ -26,94 +23,78 @@ class Mod(commands.Cog, name="ModCog"):
 
     @commands.has_permissions(read_message_history=True, manage_messages=True)
     @commands.command(brief="clear a given number of messages in the given channel")
-    async def clear(self, ctx: commands.Context, nbr: int, *criterias):
-        "add command to clear a given number of messages with filters"
+    async def clear(self, ctx: commands.Context,
+                    nbr: int,
+                    person: commands.Greedy[Member],
+                    channel: Optional[TextChannel]):
+        "clear a given number of messages in the given channel"
 
-        nbr = int(nbr)+1
-        if len(criterias) == 0:
-            messages = await ctx.history(limit=nbr).flatten()
-            for msg in messages:
-                await msg.delete()
+        if channel:
+            target = channel
         else:
-            filters = {
-                "user": None,
-                "channel": None
-            }
-            for criteria in criterias:
-                obj = await utils.auto_convert_obj(self.bot, ctx, criteria)
-                if isinstance(obj, discord.User) and not filters["user"]:
-                    filters["user"] = obj
-                elif isinstance(obj, discord.TextChannel) and not filters["channel"]:
-                    filters["channel"] = obj
+            target = ctx.channel
 
-            if filters["channel"]:
-                ctx = filters["channel"]
+        check = lambda msg: msg.author in person if person else True
 
-            i = 0
-            async for msg in ctx.history():
-                print("history")
-                if i == nbr:
-                    break
+        deleted = await target.purge(limit=min(100, nbr+1), check=check)
 
-                if filters["user"] and msg.author == filters["user"]:
-                    await msg.delete()
-                    i += 1
-        await ctx.send("Clear finished! :white_check_mark:", delete_after=1.5)
-
-    @commands.command(brief="Purges a channel")
-    async def purge(self, ctx: commands.Context):
-        "add a command to purge a channel"
-
-        deleted = await ctx.channel.purge()
-        await ctx.send(f"Deleted {deleted} message(s).", delete_after=10)
+        await ctx.send(f"{len(deleted)} messages deleted! :white_check_mark:", delete_after=2)
 
     @commands.has_permissions(send_messages=True)
     @commands.command(brief="sends a given number of messages")
-    async def send(self, ctx: commands.Context, nbr: int, *, msg: str):
-        "add a command to send a given number of messages"
+    async def send(self, ctx: commands.Context, nbr: int, *, content: str):
+        "sends a given number of messages"
 
         for _ in range(int(nbr)):
-            await ctx.send(msg)
+            await ctx.send(content)
 
     @commands.has_permissions(kick_members=True)
     @commands.command(brief="kick someone from the current guild")
-    async def kick(self, ctx: commands.Context, user: discord.Member, *, reason: str):
-        "add command to kick someone from the current guild"
+    async def kick(self, ctx: commands.Context, user: Member, *, reason: str):
+        "kick someone from the current guild"
 
         await ctx.guild.kick(user, reason=reason and f"{reason} (on behalf of {ctx.author})")
         await ctx.send(f"user `{user}` kicked!")
 
     @commands.has_permissions(ban_members=True)
     @commands.command(brief="ban someone from the current guild")
-    async def ban(self, ctx: commands.Context, members: commands.Greedy[discord.Member], delete_delay: Optional[int] = 3, *, reason: Optional[str]):
-        "add command to ban someone from the current guild"
+    async def ban(self, ctx: commands.Context,
+                  members: commands.Greedy[Member],
+                  delete_delay: Optional[int] = 3, *,
+                  reason: Optional[str]):
+        "ban someone from the current guild"
 
         for member in members:
-            await member.ban(delete_message_days=delete_delay, reason=reason and f"{reason} (on behalf of {ctx.author})")
+            await member.ban(
+                delete_message_days=delete_delay,
+                reason=reason and f"{reason} (on behalf of {ctx.author})"
+            )
             await ctx.send(f"user `{member}` banned!")
 
     @commands.has_permissions(ban_members=True)
     @commands.command(brief="unban someone from the current guild")
-    async def unban(self, ctx: commands.Context, users: commands.Greedy[discord.User], *, reason: Optional[str]):
-        "add command to unban someone from the current guild"
+    async def unban(self, ctx: commands.Context,
+                    users: commands.Greedy[User], *,
+                    reason: Optional[str]):
+        "unban someone from the current guild"
 
         for user in users:
             await ctx.guild.unban(user, reason=reason and f"{reason} (on behalf of {ctx.author})")
             await ctx.send(f"user `{user}` unbanned!")
 
-    @commands.has_permissions(manage_roles=True)
-    @commands.command(brief="removes send messages rights to specified role")
-    async def lockdown(self, ctx: commands.Context, targeted_role: discord.Role):
-        "add command to removes send messages rights to specified role"
+    # @commands.has_permissions(manage_roles=True)
+    # @commands.command(brief="removes send messages rights to specified role")
+    # async def lockdown(self, ctx: commands.Context, targeted_role: Role):
+    #     "removes send messages rights to specified role"
 
-        perms = targeted_role.permissions
+    #     perms = targeted_role.permissions
 
-        if perms.send_messages:
-            perms.update(send_messages=False)
-            msg = "Lockdown mode activated."
-        else:
-            perms.update(send_messages=True)
-            msg = "Lockdown mode deactivated."
+    #     if perms.send_messages:
+    #         perms.update(send_messages=False)
+    #         msg = "Lockdown mode activated."
+    #     else:
+    #         perms.update(send_messages=True)
+    #         msg = "Lockdown mode deactivated."
 
-        await targeted_role.edit(permissions=perms)
-        await ctx.send(msg)
+    #     await targeted_role.edit(permissions=perms)
+    #     await ctx.send(msg)
